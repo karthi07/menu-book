25fc2c218e57b846e4f7eb1cf9673d18a369587f
karthick <itkarthi02@gmail.com>
2023-09-02 13:20:47 +0530

    add menu model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/menu", to: "menu#index"
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "menu#index"
 end
