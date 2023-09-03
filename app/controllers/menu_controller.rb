639a3ea3f95b4c6f1af8a534a34f330da017ed94
karthick <itkarthi02@gmail.com>
2023-09-03 11:21:02 +0530

    add devise

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,6 @@
 class MenuController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @menu = Menu.all
   end
@@ -7,7 +9,12 @@ class MenuController < ApplicationController
     return redirect_to request.referer, notice: 'No file added' if params[:file].nil?
     return redirect_to request.referer, notice: 'Only CSV files allowed' unless params[:file].content_type == 'text/csv'
 
-    MenuImporter.new.call(params[:file])
+    # MenuImporter.new.call(params[:file])
+
+    # import =  Menu.import
+    # import_file.attach(params[:file])
+
+    # ImportMenuJob.perform_async()
 
     redirect_to request.referer, notice: 'Import started...'
   end
--- app/sidekiq/import_menu_job.rb
+++ app/sidekiq/import_menu_job.rb
@@ -1,20 +1,22 @@
-class MenuImporter
-  require 'csv'
+class ImportMenuJob
+  include Sidekiq::Job
 
-  def call(file)
-    menu_items, failed_items = parse_and_build_menu(file)
+  Sidekiq.strict_args!(false)
 
-    ImportMenuJob.perform_async(menu_items)
-
+  def perform()
+    # menu = parse_and_build_menu()
+    # Menu.import(menu_items, batch_size: 1000)
+    
   end
 
-  def parse_and_build_menu(importFile)
+  def parse_and_build_menu()
+    
     opened_file = File.open(importFile)
     options = { headers: true, col_sep: ',' }
     menu_items = []
     failed_items = []
     CSV.foreach(opened_file, **options) do |row|
-      current_menu = Menu.find_by(dish_name: row['dish_name'])
+      current_menu = Menu.find_by(dish_name: row.dish_name)
       current_menu ||= Menu.new(row)
       if current_menu.valid?
         menu_items << current_menu
@@ -23,6 +25,8 @@ class MenuImporter
         failed_items << row
       end 
     end
-    [menu_items, failed_items]
+
+    Menu.import(menu_items, batch_size: 1000)
+
   end
 end
