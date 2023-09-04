16dff6c4dc9d5b0639ec642b05da7bffd9a4b3d8
karthick <itkarthi02@gmail.com>
2023-09-04 14:30:33 +0530

    fix build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
+bundle exec rake assets:precompile SKIP_CSS_BUILD=true
 bundle exec rake assets:clean
 bundle exec rake db:migrate
